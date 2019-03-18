a5a19cddcf7afd61794cc29edf973743a93ba093
yeojinhwang <hwangyj0202@gmail.com>
2019-03-18 01:40:26 +0000

    01_staticfiles setting & add image column

--- boards/models.py
+++ boards/models.py
@@ -5,6 +5,7 @@ from django.db import models
 class Board(models.Model):
     title = models.CharField(max_length=30)
     content = models.TextField()
+    image = models.ImageField(blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     
